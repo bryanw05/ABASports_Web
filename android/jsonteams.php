1d9dff7d43dddc8f21511d5e899a12bd69013850
Bryan Wong <bryanw05@hotmail.com>
2017-06-21 16:08:20 -0700

    updated

--- android/jsonteams.php
+++ android/jsonteams.php
@@ -17,27 +17,42 @@ class jsonTeams {
         
         public function __construct() {
             $this->conn = new db();
+        } 
+            
+        public function displayTeam(){
+            $result = $this->conn->getAll('SELECT * FROM teams');
+    //        $response["Teams"] = [];
+            $return = [];
+            foreach($result as $name){
+                $return[] = [
+                 'name' => $name['name'],
+                 'division' => $name['division'],
+                 'wins' => $name['wins'],
+                 'losses' => $name['losses'],
+                 'streak' => $name['current_streak'],
+                 'roster' => $name['roster']
+                ];
+               //array_push($response["Teams"], $return);
+            }
+            echo json_encode($return, JSON_PRETTY_PRINT);
         }
-    public function displayTeam(){
-        $result = $this->conn->getAll('SELECT * FROM teams');
-//        $response["Teams"] = [];
-        $return = [];
-        foreach($result as $name){
-            $return[] = [
-             'name' => $name['name'],
-             'division' => $name['division'],
-             'wins' => $name['wins'],
-             'losses' => $name['losses'],
-             'streak' => $name['current_streak'],
-             'roster' => $name['roster']
-            ];
-           //array_push($response["Teams"], $return);
+
+        public function displayDivision(){
+            $query = $this->conn->getAll('SELECT DISTINCT division FROM teams');
+            foreach($query as $div){
+                $divisions = $div['division'];
+                $retrieve = $this->conn->getAll("SELECT name FROM teams WHERE division= '$divisions' ");
+                $returnDivs = [];
+                foreach($retrieve as $returnT){
+                  $returnDivs[$divisions][] = $returnT['name'];
+                }
+            echo json_encode($returnDivs, JSON_PRETTY_PRINT);
+            }        
         }
-        echo json_encode($return, JSON_PRETTY_PRINT);
-    }
-    
 }
 
 $jsonTeams = new jsonTeams();
-$jsonTeams->displayTeam();
+//$jsonTeams->displayTeam();
+$jsonTeams->displayDivision();
+
 ?>
